cb9fa7dfd97f8765c17079f2d514e370e9025c39
Marlysson <marlysson5@gmail.com>
2017-03-22 09:53:01 -0300

    Adicionando funcionalidade de detalhar produto desejado, ver modal de edição de produto desejado e deletar produtos.

--- Product-workflow/static/javascript/app.js
+++ Product-workflow/static/javascript/app.js
@@ -2,55 +2,93 @@
 var model = new Vue({
 	el:"#app",
 	data:{
-
+		acao : "Cadastrar",
+		
 		produtos:[
 			{
 				cod:1,
 				nome:"Copo plástico",
-				valor:0.30,
+				preco:0.30.toFixed(2),
 				categoria:"Cozinha",
 				validade:"09/08/2018"
 			},
 			{
 				cod:2,
 				nome:"Caneta",
-				valor:1.20,
+				preco:1.20.toFixed(2),
 				categoria:"Papelaria",
 				validade:"10/12/2042"},
 			{
 				cod:3,
 				nome:"Papel A4",
-				valor:20.00,
+				preco:20.00.toFixed(2),
 				categoria:"Papelaria",
 				validade:"01/01/2050"
 			}],
 
 			nome : '',
-			valor :'',
+			preco :'',
 			validade:'',
 			categoria:''
-
 	},
 
 	methods :{
 		cadastrar : function(event){
 			event.preventDefault();
 
-			produto = {}
-			produto.nome = this.nome;
-			produto.valor = this.valor;
-			produto.validade = this.validade;
-			produto.categoria = this.categoria;
+			produto = {
+				nome : this.nome,
+				preco : this.preco,
+				validade : this.validade,
+				categoria : this.categoria
+			}
 
 			this.produtos.push(produto);
 
 			this.nome = '';
-			this.valor = '';
+			this.preco = '';
 			this.validade = '';
 			this.categoria = '';
-
+		
 			document.querySelector(".modal-add-item").classList.remove("modal-visible");
+		},
 
-		}
+		editar : function(produto){
+
+			this.acao = "Editar";
+
+			this.nome = produto.nome;
+			this.categoria = produto.categoria;
+			this.preco = produto.preco;
+			this.validade = produto.validade;
+
+			document.querySelector(".modal-add-item").classList.add("modal-visible");
+			
+		},
+
+		show: function(produto){
+
+			this.nome = produto.nome;
+			this.categoria = produto.categoria;
+			this.preco = produto.preco;
+			this.validade = produto.validade;
+
+		},
+
+		remover : function(produto){
+			indice = this.produtos.indexOf(produto);
+			this.produtos.splice(indice,1);
+		},
+
+		filterByCod : function(id_produto){
+
+			produto = this.produtos.filter(function(elemento){
+				return elemento.cod == id_produto;
+			});
+
+			return produto[0];
+		},
+
+
 	}
 })
